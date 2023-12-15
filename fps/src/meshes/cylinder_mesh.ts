e55038b20ccc2f9d622ac13a4a1b23fe022cdea5
NB <n.baumgaertner@hotmail.com>
2023-12-15 14:04:34 +0100

    uv ups

--- fps/src/meshes/cylinder_mesh.ts
+++ fps/src/meshes/cylinder_mesh.ts
@@ -1,4 +1,4 @@
-import { Vec3, vec3 } from "wgpu-matrix";
+import { Vec2, Vec3, vec2, vec3 } from "wgpu-matrix";
 
 // actually a pipe aka cylinder shell...
 export function CYLINDER_VERTEX_ARRAY(n = 30, rin = 0.7, rout = 1.5, height = 3, center: Vec3 = [0, 0, 0]): Float32Array {
@@ -16,15 +16,15 @@ export function CYLINDER_VERTEX_ARRAY(n = 30, rin = 0.7, rout = 1.5, height = 3,
         );
     }
 
-    let vertices = [] as number[], normals = [] as number[];
+    let vertices = [] as number[], normals = [] as number[], uvs = [] as Vec2[];
     let p0: Vec3, p1: Vec3, p2: Vec3, p3: Vec3, p4: Vec3, p5: Vec3, p6: Vec3, p7: Vec3;
     for (let i = 0; i < n - 1; i++) {
-        p0 = pts[i][0];
-        p1 = pts[i][1];
+        p0 = pts[i][0];  // top outer point
+        p1 = pts[i][1];  // bottom outer point
         p2 = pts[i][2];
         p3 = pts[i][3];
-        p4 = pts[i + 1][0];
-        p5 = pts[i + 1][1];
+        p4 = pts[i + 1][0];  // top outer point
+        p5 = pts[i + 1][1];  // bottom outer point
         p6 = pts[i + 1][2];
         p7 = pts[i + 1][3];
 
@@ -39,17 +39,47 @@ export function CYLINDER_VERTEX_ARRAY(n = 30, rin = 0.7, rout = 1.5, height = 3,
             p6[0], p6[1], p6[2], p5[0], p5[1], p5[2], p1[0], p1[1], p1[2],
 
             //outer face
-            p0[0], p0[1], p0[2], p1[0], p1[1], p1[2], p5[0], p5[1], p5[2],
-            p5[0], p5[1], p5[2], p4[0], p4[1], p4[2], p0[0], p0[1], p0[2],
+            p0[0], p0[1], p0[2], p1[0], p1[1], p1[2], p5[0], p5[1], p5[2], // top bot bot vertices
+            p5[0], p5[1], p5[2], p4[0], p4[1], p4[2], p0[0], p0[1], p0[2], // bot top top vertices
 
             //inner face
             p2[0], p2[1], p2[2], p3[0], p3[1], p3[2], p7[0], p7[1], p7[2],
             p7[0], p7[1], p7[2], p6[0], p6[1], p6[2], p2[0], p2[1], p2[2]
         ]);
 
+        // uv data
+        // top
+        let tmp = [p0, p4, p7, p7, p3, p0,];
+        for (let t of tmp) {
+            let n = [t[0], t[2]] as Vec2;
+            n = vec2.divScalar(n, rout);
+            n = vec2.addScaled(vec2.create(0.5, 0.5), n, 0.5);
+            uvs.push(n);
+        }
+        // bottom
+        tmp = [p1, p2, p6, p6, p5, p1];
+        for (let t of tmp) {
+            let n = [t[2], t[0]] as Vec2;
+            n = vec2.divScalar(n, rout);
+            n = vec2.addScaled(vec2.create(0.5, 0.5), n, 0.5);
+            uvs.push(n);
+        }
+        // outer
+        uvs.push([0, 0]);
+        uvs.push([0, 1]);
+        uvs.push([1, 1]);
+        uvs.push([1, 1]);
+        uvs.push([1, 0]);
+        uvs.push([0, 0]);
+        // inner
+        uvs.push([1, 1]);
+        uvs.push([1, 0]);
+        uvs.push([0, 0]);
+        uvs.push([0, 0]);
+        uvs.push([0, 1]);
+        uvs.push([1, 1]);
+
         //normal data
-
-
         normals.push(...[
             //top face
             0, 1, 0, 0, 1, 0, 0, 1, 0,
@@ -57,9 +87,9 @@ export function CYLINDER_VERTEX_ARRAY(n = 30, rin = 0.7, rout = 1.5, height = 3,
 
             //bottom face
             0, -1, 0, 0, -1, 0, 0, -1, 0,
-            0, -1, 0, 0, -1, 0, 0, -1, 0,          
+            0, -1, 0, 0, -1, 0, 0, -1, 0,
         ]);
-        
+
         let outerTangent = [p0[0] - p4[0], 0, p0[2] - p4[2]];
         let outerNormal = vec3.normalize([-outerTangent[2], 0, outerTangent[0]]);
         let innerNormal = vec3.mulScalar(outerNormal, -1);
@@ -75,9 +105,10 @@ export function CYLINDER_VERTEX_ARRAY(n = 30, rin = 0.7, rout = 1.5, height = 3,
     let data: number[] = [];
     for (let i = 0; i < vertices.length; i = i + chunkSize) {
         data.push(...vertices.slice(i, i + chunkSize));
-        data.push(...[1, 1, 0, 0, 1, 0, 1]); // appending for float32x4, float32x4 ,float32x2, ...
+        data.push(...[1, 1, 0, 0, 1]); // appending 4. coordinate and color (float32x4, float32x4)
+        data.push(...uvs[i / chunkSize]); // appending uv (float32x2)
         data.push(...normals.slice(i, i + chunkSize));
-        data.push(...[1]); // ... float32x4 format 
+        data.push(...[1]); // appending 4. coordinate 
     }
     return new Float32Array(data);
 }
