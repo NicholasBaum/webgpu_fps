972d22c0cbda662e685b9abc5717fcfbe3315181
NB <n.baumgaertner@hotmail.com>
2023-12-30 21:19:43 +0100

    added transform methods

--- fps/src/core/ModelInstance.ts
+++ fps/src/core/ModelInstance.ts
@@ -3,4 +3,21 @@ import { ModelAsset } from "./modelAsset";
 
 export class ModelInstance {
     constructor(public name: string, public readonly asset: ModelAsset, public transform: Mat4 = mat4.identity()) { }
+
+    translate(x: number, y: number, z: number): ModelInstance {
+        mat4.translate(this.transform, [x, y, z], this.transform)
+        return this;
+    }
+
+    rotate(x: number, y: number, z: number): ModelInstance {
+        mat4.rotateX(this.transform, x / 180 * Math.PI, this.transform);
+        mat4.rotateY(this.transform, y / 180 * Math.PI, this.transform);
+        mat4.rotateZ(this.transform, z / 180 * Math.PI, this.transform);
+        return this;
+    }
+
+    scale(x: number, y: number, z: number): ModelInstance {
+        mat4.scale(this.transform, [x, y, z], this.transform)
+        return this;
+    }
 }
--- fps/src/scenes/SimpleScene.ts
+++ fps/src/scenes/SimpleScene.ts
@@ -1,4 +1,4 @@
-import { Vec4, mat4 } from "wgpu-matrix";
+import { Vec4 } from "wgpu-matrix";
 import { Scene } from "../core/scene";
 import { ModelInstance } from "../core/modelInstance";
 import { WASDCamera } from "../core/camera/wasdCamera";
@@ -18,16 +18,21 @@ export class SimpleScene extends Scene {
         let cylinder_asset = CREATE_CYLINDER(100, true, [0, 0, 0.5, 1], [1, 1, 1, 1], 50);
         let cylinder_asset2 = CREATE_CYLINDER(5, false, [0, 0.5, 0, 1]);
 
-        let cube = new ModelInstance(`Cube01`, cube_asset, mat4.rotateY(mat4.translation([0, 0, -50]), 0.0 * Math.PI));
-        mat4.scale(cube.transform, [10, 10, 10], cube.transform);
+        let cube = new ModelInstance(`Cube01`, cube_asset)
+            .translate(0, 0, -50)
+            .rotate(0, 30, 0)
+            .scale(10, 10, 10);
         this.models.push(cube);
 
-        let cylinder = new ModelInstance(`Cylinder01`, cylinder_asset, mat4.rotateZ(mat4.translation([20, 0, -20]), 0.25 * Math.PI));
-        mat4.scale(cylinder.transform, [10, 10, 10], cylinder.transform);
+        let cylinder = new ModelInstance(`Cylinder01`, cylinder_asset)
+            .translate(20, 0, -20)
+            .rotate(0, 0, 45)
+            .scale(10, 10, 10);
         this.models.push(cylinder);
 
-        let cylinder2 = new ModelInstance(`Cylinder01`, cylinder_asset2, mat4.translation([-20, 0, -20]));
-        mat4.scale(cylinder2.transform, [10, 10, 10], cylinder2.transform);
+        let cylinder2 = new ModelInstance(`Cylinder01`, cylinder_asset2)
+            .translate(-20, 0, -20)
+            .scale(10, 10, 10);
         this.models.push(cylinder2);
 
         this.models.push(this.light.model);
