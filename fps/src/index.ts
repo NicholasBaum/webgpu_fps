c0680f09e3380d60a38a44d501f8fbded0374bed
NB <n.baumgaertner@hotmail.com>
2024-01-14 21:32:20 +0100

    ups

--- fps/src/index.ts
+++ fps/src/index.ts
@@ -1,10 +1,21 @@
 import { Engine } from "./core/engine";
+import { addCheckBox, createRow } from "./helper/htmlBuilder";
 import { NormalMappingScene } from "./scenes/normalMappingScene";
+import { ShadowMapScene } from "./scenes/shadowMapScene";
 import { SimpleScene } from "./scenes/simpleScene";
 
 const canvas = document.querySelector("canvas")!;
-const engine = new Engine(new NormalMappingScene(), canvas);
-//const engine = new Engine(new SimpleScene(), canvas);
-engine.scene.attachUi(canvas);
-
+const scene = new ShadowMapScene();
+scene.attachUi(canvas);
+const engine = new Engine(scene, canvas);
+addEngineUI();
 await engine.run();
+
+
+function addEngineUI() {
+    const row = createRow();
+    scene.uiContainer.appendChild(row);
+    addCheckBox(row, 'show_shadow_map', (checkbox) => {
+        engine.showShadowMap = checkbox.checked;
+    }, false);
+}
