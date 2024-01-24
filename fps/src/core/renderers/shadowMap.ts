f7efbf06c48f09557868205f43254ec465878f3c
NB <n.baumgaertner@hotmail.com>
2024-01-24 02:24:40 +0100

    refactor

--- fps/src/core/renderers/shadowMap.ts
+++ fps/src/core/renderers/shadowMap.ts
@@ -31,7 +31,18 @@ export function createAndAssignShadowMap(device: GPUDevice, scene: Scene, size: 
             baseArrayLayer: index,
             arrayLayerCount: 1,
         });
-        const sm = new ShadowMap(index, smView, light, bb);
+
+        let sm: ShadowMap;
+        switch (light.type) {
+            case LightType.Direct:
+                sm = new DirectShadowMap(index, smView, light, bb);
+                break;
+            case LightType.Target:
+                sm = new TargetShadowMap(index, smView, light);
+                break;
+            default:
+                throw new Error(`Can't create a shadow map for type ${LightType[light.type]}`);
+        }
         sm.createViewMat();
         views.push(sm);
         light.shadowMap = sm;
@@ -39,17 +50,9 @@ export function createAndAssignShadowMap(device: GPUDevice, scene: Scene, size: 
     return { textureArray, views, textureSize: size };
 }
 
-
-class MatrixForwardingCamera implements ICamera {
-    constructor(private map: ShadowMap) { }
-    get view(): Mat4 { return this.map.view_mat; }
-    get projectionMatrix(): Mat4 { return this.map.proj_mat; }
-    get position(): Vec4 { return new Float32Array([...this.map.lightPosition, 1]); }
-}
-
 // class holding the shadow map textures view for a light 
 // and update logic
-export class ShadowMap {
+export abstract class ShadowMap {
 
     public camera: ICamera;
     public get lightPosition() { return this.light.position; }
@@ -60,38 +63,33 @@ export class ShadowMap {
     constructor(
         public readonly id: number,
         public readonly textureView: GPUTextureView,
-        private readonly light: Light,
-        private readonly boundingBox: BoundingBox
+        protected readonly light: Light,
     ) {
         this.camera = new MatrixForwardingCamera(this);
     }
 
-    public createViewMat() {
-        switch (this.light.type) {
-            case LightType.Direct:
-                this.createViewMatDirectLight();
-                break;
-            case LightType.Target:
-                this.createViewMatTargetLight();
-                break;
-        }
+    public abstract createViewMat(): void;
+}
+
+class MatrixForwardingCamera implements ICamera {
+    constructor(private map: ShadowMap) { }
+    get view(): Mat4 { return this.map.view_mat; }
+    get projectionMatrix(): Mat4 { return this.map.proj_mat; }
+    get position(): Vec4 { return new Float32Array([...this.map.lightPosition, 1]); }
+}
+
+class DirectShadowMap extends ShadowMap {
+
+    constructor(
+        id: number,
+        textureView: GPUTextureView,
+        light: Light,
+        private boundingBox: BoundingBox
+    ) {
+        super(id, textureView, light);
     }
 
-    private createViewMatTargetLight() {
-        const pos = this.light.position;
-        const targetPos = this.light.target;
-        mat4.lookAt(pos, targetPos, [0, 1, 0], this.view_mat);
-
-        const fov = (this.light.cutoffInDeg / 180) * 3.14;
-        const aspect = 1;
-        const near = 1;
-        const far = 100000.0;
-        mat4.perspective(fov, aspect, near, far, this.proj_mat);
-
-        mat4.multiply(this.proj_mat, this.view_mat, this.light_mat);
-    }
-
-    private createViewMatDirectLight() {
+    public override createViewMat() {
         // calculating a good spot for the directional light view
         // by using the scenes bounding box
         const bb = this.boundingBox;
@@ -109,6 +107,31 @@ export class ShadowMap {
         const near = 0;
         const far = -bb_lightSpace.min[2];
         mat4.ortho(left, right, bottom, top, near, far, this.proj_mat);
+
+        mat4.multiply(this.proj_mat, this.view_mat, this.light_mat);
+    }
+}
+
+class TargetShadowMap extends ShadowMap {
+
+    constructor(
+        id: number,
+        textureView: GPUTextureView,
+        light: Light,
+    ) {
+        super(id, textureView, light);
+    }
+
+    public override createViewMat() {
+        const pos = this.light.position;
+        const targetPos = this.light.target;
+        mat4.lookAt(pos, targetPos, [0, 1, 0], this.view_mat);
+
+        const fov = (this.light.cutoffInDeg / 180) * 3.14;
+        const aspect = 1;
+        const near = 1;
+        const far = 100000.0;
+        mat4.perspective(fov, aspect, near, far, this.proj_mat);
 
         mat4.multiply(this.proj_mat, this.view_mat, this.light_mat);
     }
