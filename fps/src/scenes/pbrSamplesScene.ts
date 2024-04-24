17ac5474d32a259f96935dff058ddc0b5cb92cd3
NB <n.baumgaertner@hotmail.com>
2024-04-25 01:41:35 +0200

    ups

--- fps/src/scenes/pbrSamplesScene.ts
+++ fps/src/scenes/pbrSamplesScene.ts
@@ -38,12 +38,7 @@ export class PbrSamplesScene extends Scene {
         let useFalloff = true;
         this.lights.push(new Light({ type: LightType.Direct, direction: [0, -1, 0], diffuseColor: [1, 1, 1, 1], intensity: 1, useFalloff: false }));
         this.lights.push(new Light({ type: LightType.Point, position: [0, 100, 100], diffuseColor: [1, 1, 1, 1], intensity, useFalloff }));
-        this.lights.push(new Light({ type: LightType.Target, position: [0, 130, 0], direction: [0.5, -1, 0], diffuseColor: [1, 1, 1, 1], intensity: 1, useFalloff: useFalloff }));
-
-        // this.lights.push(new Light({ type: LightType.Point, position: [100, 250, 100], diffuseColor: [1, 1, 1, 1], intensity, useFalloff }));
-        // this.lights.push(new Light({ type: LightType.Point, position: [-100, 250, 100], diffuseColor: [1, 1, 1, 1], intensity, useFalloff }));
-        // this.lights.push(new Light({ type: LightType.Point, position: [-100, 50, 100], diffuseColor: [1, 1, 1, 1], intensity, useFalloff }));
-        // this.lights.push(new Light({ type: LightType.Point, position: [100, 50, 100], diffuseColor: [1, 1, 1, 1], intensity, useFalloff }));
+        this.lights.push(new Light({ type: LightType.Target, position: [0, 130, 0], direction: [0.5, -1, 0], diffuseColor: [1, 1, 1, 1], intensity: 1, useFalloff: useFalloff }));  
 
         let floor = createCube(`Floor`, woodfloor)
             .translate(0, -1, 0)
