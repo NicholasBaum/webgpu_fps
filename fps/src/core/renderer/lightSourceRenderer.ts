16f8991971a82b1bce129864b85d77cb4e217f30
NB <n.baumgaertner@hotmail.com>
2024-04-29 05:01:17 +0200

    cleanup

--- fps/src/core/renderer/lightSourceRenderer.ts
+++ fps/src/core/renderer/lightSourceRenderer.ts
@@ -3,7 +3,7 @@ import { getCubeModelData } from "../../meshes/modelFactory";
 import { ICamera } from "../camera/camera";
 import { Light } from "../light";
 import { NewPipeBuilder } from "./newPipeBuilder";
-import { BindGroupDefinition, BufferDefinition } from "./bindGroupDefinition";
+import { BindGroupDefinition } from "./bindGroupDefinition";
 import { BufferObject } from "../primitives/bufferObject";
 import { BindGroupProvider } from "./bindGroupProvider";
 import { IBufferObject } from "../primitives/bufferObjectBase";
@@ -16,58 +16,53 @@ export async function createLightSourceRenderer(device: GPUDevice, lights: Light
 export class LightSourceRenderer {
 
     private _pipeBuilder;
-    private bufferBindings;
-    private vbo;
-    private builder?: BindGroupProvider;
-    private buffers?: IBufferObject[];
+    private _vbo;
+    private _buffers: IBufferObject[];
+    private _bindGroup?: GPUBindGroup;
 
     constructor(
         private lights: Light[],
         private camera: ICamera
     ) {
-        this.vbo = getCubeModelData().vertexBuffer;
-        this.bufferBindings = new BindGroupDefinition([
-            new BufferDefinition({ type: 'uniform' }),
-            new BufferDefinition({ type: 'read-only-storage' }),
-            new BufferDefinition({ type: 'read-only-storage' })
-        ]);
-
-        this._pipeBuilder = new NewPipeBuilder(SHADER)
-            .setVertexBufferLayouts(this.vbo.layout, this.vbo.topology)
-            .addBindGroup(this.bufferBindings);
-    }
-
-    async buildAsync(device: GPUDevice) {
-        await this._pipeBuilder.buildAsync(device);
-
-        this.vbo.writeToGpu(device);
+        this._vbo = getCubeModelData().vertexBuffer;
 
         const colors = this.lights.map(x => new Float32Array(x.diffuseColor));
         const transforms = () => this.lights.map(x => mat4.uniformScale(mat4.translation([...x.position, 0]), 0.5) as Float32Array);
 
-        this.buffers = [
+        this._buffers = [
             new BufferObject(() => mat4.multiply(this.camera.projectionMatrix, this.camera.view) as Float32Array, GPUBufferUsage.UNIFORM),
             new BufferObject(colors, GPUBufferUsage.STORAGE),
             new BufferObject(transforms, GPUBufferUsage.STORAGE)
         ];
 
-        this.buffers.forEach(x => x.writeToGpu(device));
-        this.builder = new BindGroupProvider(device, this._pipeBuilder.actualPipeline!)
-            .addBuffer(...this.buffers);
+        this._pipeBuilder = new NewPipeBuilder(SHADER)
+            .setVertexBufferLayouts(this._vbo.layout, this._vbo.topology)
+            .addBindGroup(
+                new BindGroupDefinition()
+                    .addBuffer('uniform')
+                    .addBuffer('read-only-storage')
+                    .addBuffer('read-only-storage')
+            );
+    }
 
-        this.builder.createBindGroups();
-
+    async buildAsync(device: GPUDevice) {
+        await this._pipeBuilder.buildAsync(device);
+        this._vbo.writeToGpu(device);
+        this._buffers.forEach(x => x.writeToGpu(device));
+        let builder = new BindGroupProvider(device, this._pipeBuilder.actualPipeline!)
+            .addBuffer(...this._buffers);
+        this._bindGroup = builder.createBindGroups()[0];
         return this;
     }
 
     render(device: GPUDevice, pass: GPURenderPassEncoder, instanceCount?: number | undefined): void {
         if (!this._pipeBuilder.actualPipeline)
             throw new Error(`Pipeline hasn't been built.`);
-        this.buffers!.forEach(x => x.writeToGpu(device));
-        pass.setVertexBuffer(0, this.vbo.buffer);
-        this.builder!.getBindGroups().forEach((x, i) => pass.setBindGroup(i, x));
+        this._buffers.forEach(x => x.writeToGpu(device));
+        pass.setVertexBuffer(0, this._vbo.buffer);
+        pass.setBindGroup(0, this._bindGroup!);
         pass.setPipeline(this._pipeBuilder.actualPipeline);
-        pass.draw(this.vbo.vertexCount, instanceCount ?? this.lights.length);
+        pass.draw(this._vbo.vertexCount, instanceCount ?? this.lights.length);
     }
 }
 
