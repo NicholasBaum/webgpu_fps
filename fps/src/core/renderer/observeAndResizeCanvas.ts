8e358dfc33d80fe778bed88364861502fc1671d8
NB <n.baumgaertner@hotmail.com>
2024-05-01 02:40:53 +0200

    added HDDPI function

--- fps/src/core/renderer/observeAndResizeCanvas.ts
+++ fps/src/core/renderer/observeAndResizeCanvas.ts
@@ -11,3 +11,36 @@ export function observeAndResizeCanvas(canvas: HTMLCanvasElement) {
     observer.observe(canvas)
     return observer;
 }
+
+export function observeAndResizeCanvasHDDPI(canvas: HTMLCanvasElement, device: GPUDevice): ResizeObserver
+export function observeAndResizeCanvasHDDPI(canvas: HTMLCanvasElement, maxTextureDimension2D: number): ResizeObserver
+export function observeAndResizeCanvasHDDPI(canvas: HTMLCanvasElement, arg2: number | GPUDevice): ResizeObserver {
+
+    let maxTextureDimension2D = arg2 instanceof GPUDevice ? arg2.limits.maxTextureDimension2D : arg2;
+
+    const observer = new ResizeObserver(([entry]) => {
+        requestAnimationFrame(() => {
+            const { width, height } = getDevicePixelContentBoxSize(entry);
+            canvas.width = Math.max(1, Math.min(width, maxTextureDimension2D));
+            canvas.height = Math.max(1, Math.min(height, maxTextureDimension2D));
+        })
+    });
+    observer.observe(canvas);
+    return observer;
+}
+
+function getDevicePixelContentBoxSize(entry: ResizeObserverEntry) {
+    // Safari does not support devicePixelContentBoxSize
+    if (entry.devicePixelContentBoxSize) {
+        return {
+            width: entry.devicePixelContentBoxSize[0].inlineSize,
+            height: entry.devicePixelContentBoxSize[0].blockSize,
+        };
+    } else {
+        // These values not correct but they're as close as you can get in Safari
+        return {
+            width: entry.contentBoxSize[0].inlineSize * devicePixelRatio,
+            height: entry.contentBoxSize[0].blockSize * devicePixelRatio,
+        };
+    }
+}
