d06f26b3d9f8a0ed8dd033fa2b9fea1dbb386ac5
NB <n.baumgaertner@hotmail.com>
2024-05-01 18:49:25 +0200

    refactor

--- fps/src/core/engineUI.ts
+++ fps/src/core/engineUI.ts
@@ -3,161 +3,165 @@ import { Engine } from "./engine";
 import { LightType } from "./light";
 import { Scene } from "./scene";
 
-export type SceneSource = { name: string, build: () => Scene }
+export type SceneBuilder = { name: string, build: () => Scene }
 
-export class EngineUI {
+export class UIController {
 
     engine!: Engine;
-    currentScene!: SceneSource;
+    currentScene?: SceneBuilder;
+
+    get uiContainer() {
+        if (!this._uiContainer) {
+            this._uiContainer = createRow('ui-container')
+            document.body.insertBefore(this._uiContainer, this.canvas.nextSibling)
+        }
+        return this._uiContainer
+    }
+    private _uiContainer?: HTMLElement;
 
     constructor(
-        public canvas: HTMLCanvasElement,
-        public scenes: SceneSource[],
+        private canvas: HTMLCanvasElement,
+        public scenes: SceneBuilder[],
     ) { }
 
-    async loadSceneAsync(scene: SceneSource): Promise<void> {
-        this.currentScene = scene;
+    async loadSceneAsync(sceneBuilder: SceneBuilder): Promise<void> {
         this.engine?.destroy();
-        // reset layout
+        this.currentScene = sceneBuilder;
+        this.engine = new Engine(sceneBuilder.build(), this.canvas);
+        await this.engine.runAsync()
+        attachUi(this);
+        // force layout reset
         this.canvas.height = 10;
         this.canvas.width = 10;
         document.body.offsetWidth;
-        this.engine = new Engine(scene.build(), this.canvas);
-        await this.engine.runAsync()
-        this.rebuild();
+    }
+}
+
+function attachUi(controller: UIController) {
+    let parent = controller.uiContainer;
+    if (parent)
+        parent.innerHTML = '';
+
+    // left menu
+    const leftMenu = parent.appendChild(createColumn());
+
+    const sceneEl = leftMenu.appendChild(createColumn());
+    insertSceneUI(sceneEl, controller.engine.scene, "Options");
+
+    const engineEL = leftMenu.appendChild(createColumn());
+    insertEngineUI(engineEL, controller.engine, "Renderer");
+
+    // right menu
+    const rightMenu = parent.appendChild(createColumn({ margin: '0px 200px 0px auto' }))
+    insertSceneSelection(rightMenu, controller, "Scenes");
+}
+
+
+function insertSceneUI(container: HTMLDivElement, scene: Scene, name?: string): void {
+    if (name)
+        addTitle(container, name);
+    let ui = createColumn();
+    container.appendChild(ui);
+    container = ui;
+    const row = createRow();
+    ui.appendChild(row);
+
+    for (let [i, l] of scene.lights.entries()) {
+        addCheckBox(row, `${LightType[l.type]}Light_${i.toString().padStart(2, '0')}`, (checkbox) => {
+            l.isOn = checkbox.checked;
+        }, l.isOn);
     }
 
-    private _parent?: HTMLElement;
-    private get parent() {
-        return this._parent ??
-            (this._parent =
-                document.body.insertBefore(createRow('ui-container'), this.canvas.nextSibling)
-            );
-    }
+    addCheckBox(row, 'isAnimated', (checkbox) => {
+        scene.isAnimated = checkbox.checked;
+    }, scene.isAnimated);
 
-    rebuild() {
-        if (this.parent)
-            this.parent.innerHTML = '';
-        const parent = this.parent;
+    const row2 = createRow();
+    ui.appendChild(row2);
 
-        const leftRoot = parent.appendChild(createColumn());
+    addCheckBox(row2, 'ambient', (checkbox) => {
+        for (let l of scene.lights.values())
+            l.disableAmbientColor = !checkbox.checked;
+    });
 
-        const configDiv = leftRoot.appendChild(createColumn());
-        addTitle(configDiv, "Options");
-        this.addOptions(configDiv);
+    addCheckBox(row2, 'diffuse', (checkbox) => {
+        for (let l of scene.lights.values())
+            l.disableDiffuseColor = !checkbox.checked;
+    });
 
-        const engineDiv = leftRoot.appendChild(createColumn());
-        addTitle(engineDiv, "Renderer");
-        this.addRendererControls(engineDiv);
+    addCheckBox(row2, 'specular', (checkbox) => {
+        for (let l of scene.lights.values())
+            l.disableSpecularColor = !checkbox.checked;
+    });
 
-        const rightRoot = parent.appendChild(createColumn({ margin: '0px 200px 0px auto' }))
+    addCheckBox(row2, 'normal_mapping', (checkbox) => {
+        for (let m of scene.models)
+            m.material.disableNormalMap = !checkbox.checked;
+    });
+}
 
-        const scenesDiv = rightRoot.appendChild(createColumn());
-        addTitle(scenesDiv, "Scenes");
-        this.addScenesSelection(scenesDiv, this.currentScene);
-    }
+function insertEngineUI(container: HTMLDivElement, engine: Engine, name?: string) {
+    if (!engine.scene.lights.some(x => x.useShadowMap && !engine.scene.environmentMap))
+        return;
+    if (name)
+        addTitle(container, name);
 
-    public addOptions(container: HTMLDivElement): void {
-        let scene = this.engine.scene;
-        let ui = createColumn();
-        container.appendChild(ui);
-        container = ui;
-        const row = createRow();
-        ui.appendChild(row);
+    // reference to every control
+    let selectedCB: HTMLInputElement | undefined = undefined;
+    let mapCB = new Array<HTMLInputElement>();
+    let viewCB = new Array<HTMLInputElement>();
+    let environmentCB: HTMLInputElement | undefined = undefined;
+    let irradianceCB: HTMLInputElement | undefined = undefined;
+    let prefilteredCB: HTMLInputElement | undefined = undefined;
+    let brdfCB: HTMLInputElement | undefined = undefined;
+    let specNumeric: NumericUpDown | undefined = undefined;
 
-        for (let [i, l] of scene.lights.entries()) {
-            addCheckBox(row, `${LightType[l.type]}Light_${i.toString().padStart(2, '0')}`, (checkbox) => {
-                l.isOn = checkbox.checked;
-            }, l.isOn);
+    // function to reset everything on changes
+    const refreshState = (newSelection: HTMLInputElement) => {
+        if (selectedCB && selectedCB != newSelection) {
+            selectedCB.checked = false;
         }
+        selectedCB = newSelection;
+        // reset engine
+        engine.showScene();
+        // return if the selected checkbox isn't checked
+        if (!selectedCB?.checked)
+            return;
+        // update engine depending on what checkbox is actually selected
+        mapCB.forEach((cb, i) => { if (cb == selectedCB) engine.showShadowMap(i); });
+        viewCB.forEach((cb, i) => { if (cb == selectedCB) engine.showLightView(i); });
+        if (environmentCB == selectedCB) engine.showEnvironmentMap();
+        if (irradianceCB == selectedCB) engine.showIrradianceMap();
+        if (prefilteredCB == selectedCB) engine.showEnvSpecularMap(specNumeric!.value);
+        if (brdfCB == selectedCB) engine.showBrdfMap();
+    };
 
-        addCheckBox(row, 'isAnimated', (checkbox) => {
-            scene.isAnimated = checkbox.checked;
-        }, scene.isAnimated);
-
-        const row2 = createRow();
-        ui.appendChild(row2);
-
-        addCheckBox(row2, 'ambient', (checkbox) => {
-            for (let l of scene.lights.values())
-                l.disableAmbientColor = !checkbox.checked;
-        });
-
-        addCheckBox(row2, 'diffuse', (checkbox) => {
-            for (let l of scene.lights.values())
-                l.disableDiffuseColor = !checkbox.checked;
-        });
-
-        addCheckBox(row2, 'specular', (checkbox) => {
-            for (let l of scene.lights.values())
-                l.disableSpecularColor = !checkbox.checked;
-        });
-
-        addCheckBox(row2, 'normal_mapping', (checkbox) => {
-            for (let m of scene.models)
-                m.material.disableNormalMap = !checkbox.checked;
-        });
-    }
-
-    addRendererControls(container: HTMLDivElement) {
-        const engine = this.engine;
-
-        // shadow map on/off, show map, light view checkboxes
-        let selectedCB: HTMLInputElement | undefined = undefined;
-        let mapCB = new Array<HTMLInputElement>();
-        let viewCB = new Array<HTMLInputElement>();
-        let environmentCB: HTMLInputElement | undefined = undefined;
-        let irradianceCB: HTMLInputElement | undefined = undefined;
-        let prefilteredCB: HTMLInputElement | undefined = undefined;
-        let brdfCB: HTMLInputElement | undefined = undefined;
-        let specNumeric: NumericUpDown | undefined = undefined;
-
-        const refreshState = (newSelection: HTMLInputElement) => {
-            if (selectedCB && selectedCB != newSelection) {
-                selectedCB.checked = false;
-            }
-            selectedCB = newSelection;
-            // reset engine
-            engine.showScene();
-            // return if the selected checkbox isn't checked
-            if (!selectedCB?.checked)
-                return;
-            // update engine depending on what checkbox is actually selected
-            mapCB.forEach((cb, i) => { if (cb == selectedCB) engine.showShadowMap(i); });
-            viewCB.forEach((cb, i) => { if (cb == selectedCB) engine.showLightView(i); });
-            if (environmentCB == selectedCB) engine.showEnvironmentMap();
-            if (irradianceCB == selectedCB) engine.showIrradianceMap();
-            if (prefilteredCB == selectedCB) engine.showEnvSpecularMap(specNumeric!.value);
-            if (brdfCB == selectedCB) engine.showBrdfMap();
-        };
-
-        engine.scene.lights.filter(x => x.useShadowMap).forEach((l, i) => {
-            const row = createRow();
-            container.appendChild(row);
-            addCheckBox(row, `ShadowMap${i}`, (checkbox) => { l.showShadows = checkbox.checked; });
-            mapCB.push(addCheckBox(row, `map`, refreshState, false));
-            viewCB.push(addCheckBox(row, `view`, refreshState, false));
-        });
-
-        // environment map
-        if (engine.scene.environmentMap) {
-            const row = createRow();
-            container.appendChild(row);
-            addCheckBox(row, `Environment`, (checkbox) => { engine.showBackground = checkbox.checked; });
-            environmentCB = addCheckBox(row, 'map', refreshState, false);
-            irradianceCB = addCheckBox(row, 'irradiance', refreshState, false);
-            prefilteredCB = addCheckBox(row, 'prefilter', refreshState, false);
-            specNumeric = addNumericUpDown(row, 0, engine.scene.environmentMap.specularMipsCount - 1, 0, 1, x => engine.showEnvSpecularMap(x))
-            brdfCB = addCheckBox(row, 'brdf', refreshState, false);
-        }
-    }
-
-    addScenesSelection(container: HTMLDivElement, initial: SceneSource) {
+    // light view controls
+    engine.scene.lights.filter(x => x.useShadowMap).forEach((l, i) => {
         const row = createRow();
         container.appendChild(row);
+        addCheckBox(row, `ShadowMap${i}`, (checkbox) => { l.showShadows = checkbox.checked; });
+        mapCB.push(addCheckBox(row, `map`, refreshState, false));
+        viewCB.push(addCheckBox(row, `view`, refreshState, false));
+    });
 
-        addSelectList(row, this.scenes, x => x.name, async (i) => {
-            await this.loadSceneAsync(this.scenes[i]);
-        }, initial);
+    // environment map controls
+    if (engine.scene.environmentMap) {
+        const row = createRow();
+        container.appendChild(row);
+        addCheckBox(row, `Environment`, (checkbox) => { engine.showBackground = checkbox.checked; });
+        environmentCB = addCheckBox(row, 'map', refreshState, false);
+        irradianceCB = addCheckBox(row, 'irradiance', refreshState, false);
+        prefilteredCB = addCheckBox(row, 'prefilter', refreshState, false);
+        specNumeric = addNumericUpDown(row, 0, engine.scene.environmentMap.specularMipsCount - 1, 0, 1, x => engine.showEnvSpecularMap(x))
+        brdfCB = addCheckBox(row, 'brdf', refreshState, false);
     }
+}
+
+function insertSceneSelection(container: HTMLDivElement, controller: UIController, name?: string) {
+    if (name)
+        addTitle(container, name);
+    addSelectList(container, controller.scenes, x => x.name, async (i) => {
+        await controller.loadSceneAsync(controller.scenes[i]);
+    }, controller.currentScene);
 }
--- fps/src/index.ts
+++ fps/src/index.ts
@@ -1,32 +1,26 @@
-import { Engine } from "./core/engine";
-import { EngineUI, SceneSource } from "./core/engineUI";
-//import { NormalMappingDebugScene } from "./scenes/normalMappingDebugScene";
+import { UIController, SceneBuilder } from "./core/engineUI";
 import { EnvironmentMapScene } from "./scenes/environmentMapScene";
 import { NormalMappingScene } from "./scenes/normalMappingScene";
 import { PbrSamplesScene } from "./scenes/pbrSamplesScene";
 import { PbrScene } from "./scenes/pbrScene";
 import { ShadowMapScene } from "./scenes/shadowMapScene";
 import { SimpleScene } from "./scenes/simpleScene";
-import { SphereScene } from "./scenes/sphereScene";
 import { TargetLightScene } from "./scenes/targetLightScene";
 import { InstancingBenchmark } from "./scenes/instancingBenchmark";
+import { NormalMappingDebugScene } from "./scenes/NormalMappingDebugScene";
 
-const scenes: SceneSource[] = [    
+const scenes: SceneBuilder[] = [
     //{ name: "Debug Scene", build: () => new NormalMappingDebugScene() },
     { name: "Pbr Samples", build: () => new PbrSamplesScene() },
     { name: "Pbr", build: () => new PbrScene() },
     { name: "Benchmark", build: () => new InstancingBenchmark() },
-    { name: "Environment Map", build: () => new EnvironmentMapScene() },
-    //{ name: "Sphere", build: () => new SphereScene() },
+    { name: "Environment Map", build: () => new EnvironmentMapScene() },    
     { name: "Target Light", build: () => new TargetLightScene() },
     { name: "Shadow Map", build: () => new ShadowMapScene() },
     { name: "Normal Map", build: () => new NormalMappingScene() },
     { name: "Simple", build: () => new SimpleScene() },
 ];
 
-const currentScene = scenes[0];
+const currentScene = scenes[4];
 const canvas = document.querySelector("canvas")!;
-const engine = new Engine(currentScene.build(), canvas);
-await engine.run();
-const ui = new EngineUI(engine, canvas, scenes, currentScene);
-ui.rebuild();
+await (new UIController(canvas, scenes)).loadSceneAsync(currentScene);
