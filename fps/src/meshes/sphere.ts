841060a64dd05650c95f97f75be2f94f564f1993
NB <n.baumgaertner@hotmail.com>
2024-05-04 00:52:05 +0200

    some rewrite

--- fps/src/meshes/sphere.ts
+++ fps/src/meshes/sphere.ts
@@ -1,37 +1,34 @@
-import { Vec3, vec3 } from "wgpu-matrix";
+import { Vec2, Vec3, Vec4, vec3 } from "wgpu-matrix";
 
 export function createSphereVertexData(numSegments: number, smooth: boolean = true, radius: number = 1): Float32Array {
     const data = createSphereVertices(numSegments, smooth, radius);
-    const appendedData = remap([4, 2, 4], data.positions, data.uvs, data.normals);
+    const appendedData = zipFlattenArrays(data.positions, 1, data.uvs, data.normals, 1);
     return new Float32Array(appendedData);
 }
 
-// strides is the number of floats each datasets type has
-function remap(strides: number[], ...data: Array<number[]>): number[] {
-    // validate input
-    if (strides.length != data.length)
-        throw new RangeError("strides lenght has to equal the amount of datasets");
-    const vertCount = data[0].length / strides[0];
-    data.forEach((d, i) => {
-        if (d.length != vertCount * strides[i])
-            throw new Error(`dataset ${i} has size ${d.length} but should have ${vertCount * strides[i]}`);
-    });
-
-    // remap
-    const mapped: number[] = [];
-    for (let i = 0; i < vertCount; i++) {
-        strides.forEach((stride, j) => {
-            mapped.push(...(data[j].slice(i * stride, i * stride + stride)));
-        })
+function zipFlattenArrays(...datasets: Array<number | Vec2[] | Vec3[] | Vec4[]>) {
+    const arrays = datasets
+        .filter(x => typeof x != 'number')
+        .map(x => x as Vec2[] | Vec3[] | Vec4[]);
+    if (!arrays.every(x => x.length == arrays[0].length))
+        throw new Error(`The datasets arrays don't have the same length.`);
+    let flattened: number[] = [];
+    for (let i = 0; i < arrays[0].length; i++) {
+        for (let j = 0; j < datasets.length; j++) {
+            let constantOrArray = datasets[j];
+            if (typeof constantOrArray == 'number')
+                flattened.push(constantOrArray);
+            else
+                flattened.push(...constantOrArray[i]);
+        }
     }
-
-    return mapped;
+    return flattened;
 }
 
 function createSphereVertices(numSegments: number, smooth: boolean = true, radius: number = 1) {
-    const positions: number[] = [];
-    const normals: number[] = [];
-    const uvs: number[] = [];
+    const positions: Vec3[] = [];
+    const normals: Vec3[] = [];
+    const uvs: Vec2[] = [];
 
     for (let i = 0; i < numSegments; i++) {
         const theta1 = (i / numSegments) * 2 * Math.PI;
@@ -47,20 +44,20 @@ function createSphereVertices(numSegments: number, smooth: boolean = true, radiu
             const v3: Vec3 = [radius * Math.sin(phi2) * Math.cos(theta2), radius * Math.cos(phi2), -radius * Math.sin(phi2) * Math.sin(theta2)];
             const v4 = [radius * Math.sin(phi2) * Math.cos(theta1), radius * Math.cos(phi2), -radius * Math.sin(phi2) * Math.sin(theta1)];
             // Push the vertices for each face
-            positions.push(...v3, 1, ...v2, 1, ...v1, 1);
-            positions.push(...v1, 1, ...v4, 1, ...v3, 1);
+            positions.push(v3, v2, v1);
+            positions.push(v1, v4, v3);
 
             const norm = vec3.normalize;
             // Calculate normals
             if (smooth) {
-                normals.push(...norm(v3), 0, ...norm(v2), 0, ...norm(v1), 0);
-                normals.push(...norm(v1), 0, ...norm(v4), 0, ...norm(v3), 0);
+                normals.push(norm(v3), norm(v2), norm(v1));
+                normals.push(norm(v1), norm(v4), norm(v3));
             } else {
                 // calc face average
                 let t1 = norm(vec3.mulScalar(vec3.add(vec3.add(v3, v2), v1), 1 / 3));
                 let t2 = norm(vec3.mulScalar(vec3.add(vec3.add(v1, v4), v3), 1 / 3));
-                normals.push(...t1, 0, ...t1, 0, ...t1, 0);
-                normals.push(...t2, 0, ...t2, 0, ...t2, 0);
+                normals.push(t1, t1, t1);
+                normals.push(t2, t2, t2);
             }
             // Calculate UV coordinates
             const uv_u1 = i / numSegments;
@@ -69,8 +66,8 @@ function createSphereVertices(numSegments: number, smooth: boolean = true, radiu
             const uv_v2 = (j + 1) / numSegments;
 
             // Push the UV coordinates for each face
-            uvs.push(uv_u2, uv_v2, uv_u2, uv_v1, uv_u1, uv_v1);
-            uvs.push(uv_u1, uv_v1, uv_u1, uv_v2, uv_u2, uv_v2);
+            uvs.push([uv_u2, uv_v2], [uv_u2, uv_v1], [uv_u1, uv_v1]);
+            uvs.push([uv_u1, uv_v1], [uv_u1, uv_v2], [uv_u2, uv_v2]);
         }
     }
 
