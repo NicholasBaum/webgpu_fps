67beeed793f63f259fa8ed2630da4fae00708a40
NB <n.baumgaertner@hotmail.com>
2024-05-03 03:49:41 +0200

    better defaults

--- fps/src/core/scene.ts
+++ fps/src/core/scene.ts
@@ -1,14 +1,14 @@
 import { Camera } from "./camera/camera";
 import { WASDCamera } from "./camera/wasdCamera";
 import { EnvironmentMap } from "./environment/environmentMap";
-import { Light } from "./light";
+import { Light, LightType } from "./light";
 import { ModelInstance } from "./modelInstance";
 
 export class Scene {
     public aspectRatio: 'auto' | 'camera' = 'auto';
     public isAnimated: boolean = true;
-    public camera: Camera = new WASDCamera();
-    public lights: Light[] = [new Light()];;
+    public camera: Camera = new WASDCamera({ position: [0, 0, 10] });
+    public lights: Light[] = [new Light({ type: LightType.Direct, direction: [-1, -2, -1] })];;
     public models: ModelInstance[] = [];
     public environmentMap: EnvironmentMap | undefined;
 
